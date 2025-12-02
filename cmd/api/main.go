b301e90980ee050d3bd16b677fa115e9e22a6c6b
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-02 11:46:35 +0700

    set up swagger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,13 +5,22 @@ import (
 	"koda-shortlink/internal/routers"
 
 	"github.com/joho/godotenv"
+	_ "koda-shortlink/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 
+// @title Koda Shortlink API
+// @version 1.0
+// @description API documentation for Koda Shortlink
+// @host localhost:8082
+// @BasePath /api/v1
 func main() {
 	godotenv.Load()
 	pg := config.InitDbConfig()
 	r := routers.InitRouter(pg)
 
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":8082")
 }
