252140fb3aa356b55995abbf2a4860c0e47eb6a9
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-03 16:14:08 +0700

    refactor: update router create link

--- internal/routers/sortlinks.router.go
+++ internal/routers/sortlinks.router.go
@@ -14,13 +14,15 @@ func ShortlinkRoutes(r *gin.Engine, pg *pgxpool.Pool) {
 	shortlinkController := handler.ShortlinkController{DB: pg}
 
 	shortlinks := r.Group("/api/v1")
-	{	
-		shortlinks.POST("/links",middleware.RateLimitMiddleware(5, 5*time.Minute) ,shortlinkController.CreateShortlink)
-		shortlinks.GET("/links", shortlinkController.GetAllShortlinks)
-		shortlinks.GET("/links/:shortCode", shortlinkController.GetShortlinkByCode)
-		shortlinks.PUT("/links/:shortCode", shortlinkController.UpdateShortlink)
-		shortlinks.DELETE("/links/:shortCode", shortlinkController.DeleteShortlink)
-		shortlinks.GET("/dashboard/stats", shortlinkController.GetDashboardStats )
+	opt := shortlinks.Group(("/"))
+	opt.Use(middleware.OptAuthMiddleware(""))
+	opt.POST("/links",middleware.RateLimitMiddleware(5, 5*time.Minute) ,shortlinkController.CreateShortlink)
+	{
+		shortlinks.GET("/links", middleware.AuthMiddleware(""),shortlinkController.GetAllShortlinks)
+		shortlinks.GET("/links/:shortCode", middleware.AuthMiddleware(""),shortlinkController.GetShortlinkByCode)
+		shortlinks.PUT("/links/:shortCode",middleware.AuthMiddleware("") ,shortlinkController.UpdateShortlink)
+		shortlinks.DELETE("/links/:shortCode", middleware.AuthMiddleware(""),shortlinkController.DeleteShortlink)
+		shortlinks.GET("/dashboard/stats", middleware.AuthMiddleware(""),shortlinkController.GetDashboardStats )
 	}
 	
 	r.GET("/:shortCode", shortlinkController.GetShortlinksRedis)
