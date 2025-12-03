e00076ac8999148a89a79341e2ca91b9966b8590
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-03 16:09:13 +0700

    feat: add option middleware

--- internal/middleware/auth.middleware.go
+++ internal/middleware/auth.middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"koda-shortlink/internal/utils"
 	"os"
 	"strings"
@@ -11,8 +12,8 @@ import (
 
 func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
 		authHeader := ctx.GetHeader("Authorization")
+		fmt.Println(authHeader)
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 			ctx.JSON(401, gin.H{"success": false, "message": "Missing or invalid Authorization header"})
 			ctx.Abort()
@@ -20,7 +21,6 @@ func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-
 		secret := os.Getenv("JWT_SECRET")
 		if secret == "" {
 			ctx.JSON(500, gin.H{"success": false, "message": "Server missing JWT secret"})
@@ -39,12 +39,57 @@ func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 			return
 		}
 
-		ctx.Set("userID", claims.Id)
+		ctx.Set("userID", int64(claims.Id))
 		ctx.Set("userEmail", claims.Email)
 		ctx.Set("userRole", claims.Role)
 
 		if requiredRole != "" && claims.Role != requiredRole {
-			ctx.JSON(403, gin.H{"success": false, "message": "Not permission"})
+			ctx.JSON(403, gin.H{"success": false, "message": "No permission"})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
+
+func OptAuthMiddleware(requiredRole string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			fmt.Println("option")
+			ctx.Next()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		secret := os.Getenv("JWT_SECRET")
+		if secret == "" {
+			ctx.JSON(500, gin.H{"success": false, "message": "Server missing JWT secret"})
+			ctx.Abort()
+			return
+		}
+
+		claims := &utils.UserPayload{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+
+		fmt.Println(token.Valid)
+
+		if err != nil || !token.Valid {
+			ctx.JSON(401, gin.H{"success": false, "message": "Invalid token"})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("userID", int64(claims.Id))
+		ctx.Set("userEmail", claims.Email)
+		ctx.Set("userRole", claims.Role)
+
+		if requiredRole != "" && claims.Role != requiredRole {
+			ctx.JSON(403, gin.H{"success": false, "message": "No permission"})
 			ctx.Abort()
 			return
 		}
