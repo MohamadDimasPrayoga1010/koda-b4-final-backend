cd8c46fcdc45edd43ee7876a945166b5ebd5d02d
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-03 16:12:51 +0700

    feat: add router profile

--- internal/routers/profile.router.go
+++ internal/routers/profile.router.go
@@ -1,26 +1,20 @@
 package routers
 
 import (
-"koda-shortlink/internal/handler"
-"koda-shortlink/internal/middleware"
+	"koda-shortlink/internal/handler"
+	"koda-shortlink/internal/middleware"
 
-
-"github.com/gin-gonic/gin"
-"github.com/jackc/pgx/v5/pgxpool"
-
-
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func UserRoutes(r *gin.Engine, pg *pgxpool.Pool) {
-profileController := handler.ProfileController{DB: pg}
-dashboardController := handler.ShortlinkController{DB: pg}
+	profileController := handler.ProfileController{DB: pg}
 
-
-user := r.Group("/api/v1")
-{
-	user.GET("/profile", middleware.AuthMiddleware(""), profileController.GetProfile)
-	user.GET("/dashboard/stats", middleware.AuthMiddleware(""), dashboardController.GetDashboardStats)
-}
-
+	user := r.Group("/api/v1")
+	{
+		user.GET("/profile", middleware.AuthMiddleware(""), profileController.GetProfile)
+		user.PATCH("/profile", middleware.AuthMiddleware(""), profileController.UpdateProfile)
+	}
 
 }
