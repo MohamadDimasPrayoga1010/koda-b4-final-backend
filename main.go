19b6e7919c78c170b190a6f4d3856bbba43bfde3
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-03 16:15:02 +0700

    setup main

--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
+// @title           API Koda Shortlink Documentation
+// @version         1.0
+// @description     Dokumentasi REST API menggunakan Gin dan Swagger
 
+// @BasePath  /
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+import (
+	"koda-shortlink/internal/config"
+	"koda-shortlink/internal/routers"
+	"koda-shortlink/internal/utils"
+
+	_ "koda-shortlink/docs"
+
+	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	godotenv.Load()
+	pg := config.InitDbConfig()
+	r := routers.InitRouter(pg)
+
+	utils.InitRedis()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8082")
+}
